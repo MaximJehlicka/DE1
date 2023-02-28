fd461ad253c1259f546da9db08b6979b935255b8
MaximJehlicka <125310309+MaximJehlicka@users.noreply.github.com>
2023-02-28 20:57:53 +0100

    Update tb_mux_3bit_4to1.vhd

--- labs/03-vivado/multiplexer1/multiplexer1.srcs/sim_1/new/tb_mux_3bit_4to1.vhd
+++ labs/03-vivado/multiplexer1/multiplexer1.srcs/sim_1/new/tb_mux_3bit_4to1.vhd
@@ -2,7 +2,7 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 22.02.2023 16:47:24
+-- Create Date: 02/23/2023 12:05:25 PM
 -- Design Name: 
 -- Module Name: tb_mux_3bit_4to1 - Behavioral
 -- Project Name: 
@@ -32,46 +32,74 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity tb_mux_3bit_4to1 is
- 
+--  Port ( );
 end tb_mux_3bit_4to1;
 
 architecture Behavioral of tb_mux_3bit_4to1 is
- port
-    signal a_i : std_logic_vector(2 downto 0);
-    signal b_i : std_logic_vector(2 downto 0);
-    signal c_i : std_logic_vector(2 downto 0);
-    signal d_i : std_logic_vector(2 downto 0);
-    signal sel_i : std_logic_vector(1 downto 0);
-    signal f_o : std_logic_vector(2 downto 0);
-    
-begin
-    -- Connecting testbench signals with comparator_4bit
-    -- entity (Unit Under Test)
-    uut_mux_3bit_4to1 : entity work.mux_3bit_4to1
-   
-      port map(
-            a_i => s_a,
-            b_i => s_b,
+
+  -- Testbench local signals
+  signal sig_d           : std_logic_vector(2 downto 0);
+  signal sig_c           : std_logic_vector(2 downto 0);
+  signal sig_b           : std_logic_vector(2 downto 0);
+  signal sig_a           : std_logic_vector(2 downto 0);
+  signal sig_sel         : std_logic_vector(1 downto 0);
+  signal sig_f_o         : std_logic_vector(2 downto 0);
   
-        );
-
 begin
 
-   -- Report a note at the beginning of stimulus process
-        report "Stimulus process started";
+    uut_mux_3bit_4to1 : entity work.mux_3bit_4to1
+    port map (
+      d_i           => sig_d,
+      c_i           => sig_c,
+      b_i           => sig_b,
+      a_i           => sig_a,
+      sel_i        => sig_sel,
+      f_o         => sig_f_o
+    );
+    
+  p_stimulus : process is
+  begin
 
-        -- First test case ...
-        sel_i <= "00";
-        s_a <= "0000";
-        wait for 100 ns;
-        -- ... and its expected outputs
-        assert (
-        (s_B_greater_A = '0') and
-        (s_B_equals_A = '1') and
-        (s_B_less_A = '0')
+    sig_d <= "001";
+    sig_c <= "010";
+    sig_b <= "011";
+    sig_a <= "111";
+    
+    sig_sel <= "00";
+    wait for 100 ns;
+    assert (
+            (sig_f_o = sig_a)
         )
-        -- If false, then report an error
-        -- If true, then do not report anything
-        report "Input combination 0000, 0000 FAILED" severity error;
+      report "Input sel=00 FAILED"
+      severity error;
+      
+
+    sig_sel <= "01";
+    wait for 100 ns;
+    assert (
+            (sig_f_o = sig_b)
+        )
+      report "Input sel=01 FAILED"
+      severity error;
+      
+      
+     sig_sel <= "10";
+    wait for 100 ns;
+    assert (
+            (sig_f_o = sig_c)
+        )
+      report "Input sel=10 FAILED"
+      severity error;
+      
+      
+     sig_sel <= "11";
+    wait for 100 ns;
+    assert (
+            (sig_f_o = sig_d)
+        )
+      report "Input sel=11 FAILED"
+      severity error;
+
+  end process p_stimulus;
 
 end Behavioral;
