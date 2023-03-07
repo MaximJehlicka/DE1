971f672ad28729ac8f05eb5bf49a75a309a377f1
MaximJehlicka <125310309+MaximJehlicka@users.noreply.github.com>
2023-03-07 22:02:33 +0100

    Update top.vhd

--- labs/04-segment/display/display.srcs/sources_1/new/top.vhd
+++ labs/04-segment/display/display.srcs/sources_1/new/top.vhd
@@ -2,7 +2,7 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 03/02/2023 11:53:41 AM
+-- Create Date: 03/01/2023 15:43:37 AM
 -- Design Name: 
 -- Module Name: top - Behavioral
 -- Project Name: 
