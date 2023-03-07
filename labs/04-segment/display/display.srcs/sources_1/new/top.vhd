76aeb55fd9b58ac2d083241ed7a6c65ce71087d7
MaximJehlicka <125310309+MaximJehlicka@users.noreply.github.com>
2023-03-07 21:43:46 +0100

    Update top.vhd

--- labs/04-segment/display/display.srcs/sources_1/new/top.vhd
+++ labs/04-segment/display/display.srcs/sources_1/new/top.vhd
@@ -2,7 +2,7 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 01.03.2023 15:56:13
+-- Create Date: 03/02/2023 11:53:41 AM
 -- Design Name: 
 -- Module Name: top - Behavioral
 -- Project Name: 
@@ -21,6 +21,7 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -45,11 +46,11 @@ entity top is
            BTNC : in STD_LOGIC);
 end top;
 
-architecture Behavioral of top is
+architecture behavioral of top is
 
 begin
 
---------------------------------------------------------------------
+  --------------------------------------------------------------------
   -- Instance (copy) of hex_7seg entity
   --------------------------------------------------------------------
 
@@ -67,26 +68,28 @@ begin
     );
 
   -- Connect one common anode to 3.3V
-  AN <= b"1111_0111";
+  AN <= b"0111_1111";
 
   -- Display input value on LEDs
   LED(3 downto 0) <= SW;
 
 --------------------------------------------------------------------
 -- Experiments on your own: LED(7:4) indicators
-
 -- Turn LED(4) on if input value is equal to 0, ie "0000"
--- LED(4)
-
+ LED(4) <= '1' when (SW = "0000") else '0';
 
 -- Turn LED(5) on if input value is greater than "1001", ie 10, 11, 12, ...
--- LED(5) <= WRITE YOUR CODE HERE
+ LED(5) <= '1' when (SW > "1001") else '0';
 
 -- Turn LED(6) on if input value is odd, ie 1, 3, 5, ...
--- LED(6) <= WRITE YOUR CODE HERE
+ LED(6) <= '1' when (SW(0) = '1') else '0';
 
+with SW select
 -- Turn LED(7) on if input value is a power of two, ie 1, 2, 4, or 8
--- LED(7) <= WRITE YOUR CODE HERE
+ LED(7) <= '1' when "0001",
+           '1' when "0010",
+           '1' when "0100",
+           '1' when "1000",
+           '0' when others;
 
-
-end Behavioral;
+end architecture behavioral;
